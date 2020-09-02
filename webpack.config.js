be1e6adcf1d7377ce811e936d67a138ca0d1a5f6
ecnuLang <1181031235@qq.com>
2020-09-02 11:01:15 +0800

    vue demo

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+module.exports = {
+    target: 'web',
+    mode: 'development',
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // 将css写入到HTML
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            //stylus-loader专门用来处理stylus文件，处理完成后变成css文件，交给css-loader.webpack的loader就是这样一级一级向上传递，每一层loader只处理自己关心的部分
+            {
+                test: /\.styl/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            //将小于1024d的图片转为base64，减少http请求
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
